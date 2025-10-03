619abe4b02c1c0a2d19b48230fa0b9cd3289d63c
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-10-03 16:19:18 -0700

    feat: agrege atributos a jugador y tablero

--- RummyEquipo01/ObjetosNegocio/src/main/java/entidades/Jugador.java
+++ RummyEquipo01/ObjetosNegocio/src/main/java/entidades/Jugador.java
@@ -1,10 +1,46 @@
 
 package entidades;
 
+import java.util.List;
+
 /**
  *
  * @author pedro
  */
 public class Jugador {
+    private String avatar;
+    private String nombre;
+    private boolean esPrimerTurno;
+    private List<Ficha> fichas;
     
+    /**
+     * metedo que regresa si es el primer turno
+     * @return regresa true si es el primer turno y false si no lo es
+     */
+    public boolean isEsPrimerTurno() {
+        return esPrimerTurno;
+    }
+    /**
+     * metodo que regresa la lista de fichas  en la mano de jugador
+     * @return regresa la lista de fichas en la mano de jugador
+     */
+    public List<Ficha> getFichas() {
+        return fichas;
+    }
+    /**
+     * elimina las fichas en las posiciones del parametro
+     * @param posiciones lista de posiciones en las cuales estan las fichas a ser eliminadas
+     */
+    public void quitarFichas(List<Integer> posiciones){
+        
+    }
+    /**
+     * elimina las fichas en las posiciones del parametro
+     * @param posiciones lista de posiciones en las cuales estan las fichas a ser eliminadas2
+     */
+    public boolean quitarFichasPrimerTurno(){
+        throw new UnsupportedOperationException();
+    }
+
 }
+
