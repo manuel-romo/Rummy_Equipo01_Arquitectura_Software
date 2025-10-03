04eb70b8fac88045c07d4430e98b5ad81304c12a
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-10-03 14:47:02 -0700

    feat: creación paquete interfaces e interfaz ITablero

---
+++ RummyEquipo01/ObjetosNegocio/src/main/java/interfaces/ITablero.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+
+public interface ITablero {
+    public abstract boolean agregarFichasTablero(int [] idFichas);
+    public abstract boolean agregarFichasTablero(int [] idsFicha, int numeroGrupo);
+    public abstract boolean quitarFichasJugador(int [] posiciones);
+    public abstract boolean quitarFichasTablero(int [] idFichas);
+    public abstract boolean seleccionarFichasTablero(int [] idFichas);
+}
