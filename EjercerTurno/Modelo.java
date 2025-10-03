d4829f3c5b255f91128db8e20163604bb798a69b
Oscar Mauricio Areiza Paramo <omareiza-2023a@corhuila.edu.co>
2025-10-02 22:44:53 -0700

    se agreo el EjercerTurno

---
+++ EjercerTurno/Modelo.java
@@ -0,0 +1,17 @@
+package EjercerTurno;
+
+public class Modelo {
+
+    public void terminarTurno() {}
+
+    public void seleccionarFichasJugador(int[] posicionFicha) {}
+
+    public void seleccionarFichasTablero(int[] posicionesFichas, int idFichaSeleccionada) {}
+
+    public void quitarFichasJugador(int[] posicionesFichas) {}
+
+    public void quitarFichasTablero(int[] idsFichas) {}
+
+    public void agregarFichasTablero(int[] idsFichas, int numeroGrupo) {}
+    
+}
