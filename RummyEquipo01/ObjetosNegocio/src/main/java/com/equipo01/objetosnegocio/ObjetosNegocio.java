c6bdb84f3a97eccd71f2f6f552a8eb5017603f8c
manuel-romo <manuel.romo253080@potros.itson.edu.mx>
2025-10-03 08:46:38 -0700

    Estructura Proyectos

---
+++ RummyEquipo01/ObjetosNegocio/src/main/java/com/equipo01/objetosnegocio/ObjetosNegocio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.equipo01.objetosnegocio;
+
+/**
+ *
+ * @author romom
+ */
+public class ObjetosNegocio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ RummyEquipo01/ObjetosPresentacion/src/main/java/com/equipo01/objetospresentacion/ObjetosPresentacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.equipo01.objetospresentacion;
+
+/**
+ *
+ * @author romom
+ */
+public class ObjetosPresentacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
