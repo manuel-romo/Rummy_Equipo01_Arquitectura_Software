d5a2f74201e7b97a9bc107f09ab99b526965ef12
manuel-romo <manuel.romo253080@potros.itson.edu.mx>
2025-10-03 09:08:59 -0700

    Creación de paquete dto de presentación

--- RummyEquipo01/ObjetosPresentacion/src/main/java/com/equipo01/ejercerturno/EjercerTurno.java
+++ RummyEquipo01/ObjetosPresentacion/src/main/java/com/equipo01/ejercerturno/EjercerTurno.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package com.equipo01.ejercerturno;
 
 /**
  *
- * @author romom
+ * @author Manuel Romo López
  */
 public class EjercerTurno {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
---
+++ RummyEquipo01/ObjetosPresentacion/src/main/java/com/equipo01/objetospresentacion/EstadoActual.java
@@ -0,0 +1,10 @@
+
+package com.equipo01.objetospresentacion;
+
+/**
+ *
+ * @author romom
+ */
+public class EstadoActual {
+    
+}
