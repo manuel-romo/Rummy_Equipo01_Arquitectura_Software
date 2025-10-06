36303bf3b275b14140d8bf1da55e929a4bef9187
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-10-05 23:28:41 -0700

    feat: se agregaron las nuevas clases dto en negocio

--- RummyEquipo01/ObjetosNegocio/src/main/java/dto/TableroNegocioDTO.java
+++ RummyEquipo01/ObjetosNegocio/src/main/java/dto/TableroNegocioDTO.java
@@ -4,10 +4,49 @@
  */
 package dto;
 
+import java.util.List;
+
 /**
  *
  * @author pedro
  */
 public class TableroNegocioDTO {
+    private JugadorNegocioDTO jugadorPrincipal;
+    private List<JugadorNegocioDTO> jugadores;
+    private List<GrupoNegocioDTO> grupoFichas;
+
+    public TableroNegocioDTO(JugadorNegocioDTO jugadorPrincipal, List<JugadorNegocioDTO> jugadores, List<GrupoNegocioDTO> grupoFichas) {
+        this.jugadorPrincipal = jugadorPrincipal;
+        this.jugadores = jugadores;
+        this.grupoFichas = grupoFichas;
+    }
+
+    public TableroNegocioDTO() {
+    }
+
+    public JugadorNegocioDTO getJugadorPrincipal() {
+        return jugadorPrincipal;
+    }
+
+    public void setJugadorPrincipal(JugadorNegocioDTO jugadorPrincipal) {
+        this.jugadorPrincipal = jugadorPrincipal;
+    }
+
+    public List<JugadorNegocioDTO> getJugadores() {
+        return jugadores;
+    }
+
+    public void setJugadores(List<JugadorNegocioDTO> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public List<GrupoNegocioDTO> getGrupoFichas() {
+        return grupoFichas;
+    }
+
+    public void setGrupoFichas(List<GrupoNegocioDTO> grupoFichas) {
+        this.grupoFichas = grupoFichas;
+    }
+    
     
 }
