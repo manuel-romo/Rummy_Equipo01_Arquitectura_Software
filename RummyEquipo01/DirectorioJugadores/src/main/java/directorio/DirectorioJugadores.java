59266339e33cb33905496e2ce1422ecb26906e10
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-11-10 12:55:44 -0700

    feat: modulo directorio

---
+++ RummyEquipo01/DirectorioJugadores/src/main/java/directorio/DirectorioJugadores.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package directorio;
+
+import interfaces.IDirectorio;
+import interfaces.IDispatcher;
+import java.util.Map;
+
+/**
+ *
+ * @author ramon
+ */
+public class DirectorioJugadores implements IDirectorio{
+    
+    private Map<String, String[]> jugadoresDirecciones;
+    private IDispatcher dispatcher;
+    
+    @Override
+    public void agregarJugador(String nombreJugador, String[] direccion) {
+        jugadoresDirecciones.put(nombreJugador, direccion);
+    }
+
+    @Override
+    public void enviarMensajeJugadores(String respuesta) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
