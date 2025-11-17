7058568d7b15087c004581feff9a8d74ee743de2
juan-pablitoINSOF <juan.heras247054@potros.itson.edu.mx>
2025-11-17 10:03:00 -0700

    feat: añadí interfaces faltantes
    En interfaz de receptorRespuestaSerializada falta añadir repuesta DTO como parametro

---
+++ RummyEquipo01/Interfaces/src/main/java/interfaces/ICommand.java
@@ -0,0 +1,10 @@
+
+package interfaces;
+
+/**
+ * Interfaz de Command para implementar en comandos concretos.
+ * @author Juan Heras
+ */
+public interface ICommand {
+    
+}
---
+++ RummyEquipo01/Interfaces/src/main/java/interfaces/IReceptorRespuestaDeserializada.java
@@ -0,0 +1,12 @@
+
+package interfaces;
+
+/**
+ * Interfaz que recibe el DTO de respuesta para usarla en el módelo 
+ * @author juanpheras
+ */
+public interface IReceptorRespuestaDeserializada {
+    
+    //AQUI FALTA AÑADIR LA RESPUESTA DTO COMO PARAMETRO 
+    public abstract void recibirRespuestaDeserializada();
+}
