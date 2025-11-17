d00cc8c132b1061ab9d6e682a607733434f8c611
ramonZamudio5 <ramon.zamudio251924@potros.itson.edu.mx>
2025-11-17 10:27:32 -0700

    feat: añadi el modulo de dtos

---
+++ RummyEquipo01/DTORummy/src/main/java/dto/TableroDTO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dto;
+
+import java.util.List;
+
+/**
+ *
+ * @author ramon
+ */
+public class TableroDTO {
+    private List<GrupoDTO> grupos;
+    private MontonDTO monton;
+}
