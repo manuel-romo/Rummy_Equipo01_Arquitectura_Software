4250c311672d017e0b200bd84b6d0820b72c53a5
pedroesquer <pedro.morales252815@potros.itson.edu.mx>
2025-11-17 12:23:25 -0700

    feat: directorio servidor

--- RummyEquipo01/Serializador/src/main/java/serializador/Serializador.java
+++ RummyEquipo01/Serializador/src/main/java/serializador/Serializador.java
@@ -1,35 +1,37 @@
-///*
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
-// */
-//package serializador;
-//
-//import interfaces.ICommand;
-//import interfaces.IFiltro;
-//
-///**
-// *
-// * @author pedro
-// */
-//public class Serializador implements IFiltro {
-//
-//    private IFiltro filtroSiguiente;
-//    private final gson = new Gson();
-//    private String serializarAccion(ICommand command) {
-//        
-//    }
-//
-//    private void enviarAccionSerializada(String accionSerializada) {
-//
-//    }
-//
-//    @Override
-//    public void ejecutar(ICommand comando) {
-//        String accion = this.serializarAccion(comando);
-//        this.enviarAccionSerializada(accion);
-//
-//        if (filtroSiguiente != null) {
-//            filtroSiguiente.ejecutar(comando);
-//        }
-//    }
-//}
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serializador;
+
+import com.google.gson.Gson;
+import interfaces.ICommand;
+import interfaces.IFiltro;
+
+/**
+ *
+ * @author pedro
+ */
+public class Serializador implements IFiltro {
+
+    private IFiltro filtroSiguiente;
+    private final Gson gson = new Gson();
+    
+    private String serializarAccion(ICommand command) {
+        return gson.toJson(command);
+    }
+
+    private void enviarAccionSerializada(String accionSerializada) {
+        
+    }
+
+    @Override
+    public void ejecutar(ICommand comando) {
+        String accion = this.serializarAccion(comando);
+        this.enviarAccionSerializada(accion);
+
+        if (filtroSiguiente != null) {
+            filtroSiguiente.ejecutar(comando);
+        }
+    }
+}
