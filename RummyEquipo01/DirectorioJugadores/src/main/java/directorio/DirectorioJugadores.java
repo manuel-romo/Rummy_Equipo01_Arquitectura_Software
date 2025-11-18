e8b97f464456a3de3dd8743eab1a5c8a5f45cfc7
juan-pablitoINSOF <juan.heras247054@potros.itson.edu.mx>
2025-11-18 01:43:05 -0700

    feat: Añadí lógica de directorioJugadores

--- RummyEquipo01/DirectorioJugadores/src/main/java/directorio/DirectorioJugadores.java
+++ RummyEquipo01/DirectorioJugadores/src/main/java/directorio/DirectorioJugadores.java
@@ -1,9 +1,9 @@
- /*
+/*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package directorio;
 
+import comandoEnvolvente.ComandoEnvolvente;
 import interfaces.ICommand;
 import interfaces.IDirectorio;
 import interfaces.IDispatcher;
@@ -14,25 +14,21 @@ import java.util.Map;
  *
  * @author ramon
  */
-public class DirectorioJugadores implements IFiltro{
-    
+public class DirectorioJugadores implements IFiltro {
+
     private Map<String, String[]> jugadoresDirecciones;
     private IDispatcher dispatcher;
-    
-    
+
     public void agregarJugador(String nombreJugador, String[] direccion) {
         jugadoresDirecciones.put(nombreJugador, direccion);
     }
 
-    
-    public void enviarMensajeJugadores(String respuesta) {
-//        String direccion = 
-        
-    }
 
     @Override
     public void ejecutar(ICommand comando) {
-//        enviarMensajeJugadores(respuesta);
+        ComandoEnvolvente envolvente = (ComandoEnvolvente) comando;
+
+        dispatcher.agregarMensaje(envolvente.getMensajeSerializado(), jugadoresDirecciones.get(comando.getNombreJugador()));
     }
 
 }
